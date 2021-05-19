b6b675035d5ba3273d53a4ee24b094718b4c947b
mwcomputer <kolbenfresser5@googlemail.com>
2021-05-19 14:41:46 +0200

    fertig

--- challenge_02.js
+++ challenge_02.js
@@ -15,3 +15,15 @@ Alter 13-17 // Cola
 */
 
 /* Getränke-Challenge */
+
+let vorname = prompt("Wie ist dein Name?");
+let age = parseInt(prompt("Wie Alt bist du?"));
+switch (true) 
+{
+    case (age >=0 && age <=5): console.log(vorname + " hat freigabe für Milch.");break;
+    case (age >=6 && age <=12): console.log(vorname +" hat freigabe für Milch und Saft.");break;
+    case (age >=13 && age <=17): console.log(vorname + " hat freigabe für Milch, Saft und Cola.");break;
+    case (age > 17): console.log(vorname + " hat freigabe für Milch, Saft, Cola und Wein.");break;
+    default: console.log("Ausgabe verweigert!!!!");
+    break;
+};
